3210ad96fbcaed102a912ebee62b15833c4a78fa
Truman Cheng <trumanhc@uci.edu>
2020-09-20 20:46:22 -0700

    add template structure:

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,23 @@
 from app import app
 from flask import render_template
 
+
 #decorators
 #associates url from arag to the function below
 @app.route('/') 
 @app.route('/index')
 
+
 def index():
     user = {'username': 'trums'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username': 'kms'},
+            'body': 'fadkfjadflkadjfdfdfa'
+        },
+        {
+            'author': {'username': 'pepega'},
+            'body': 'fadkfjadflkadjfdfdfa'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
