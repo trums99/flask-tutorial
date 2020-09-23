3cee5a745ef50b8b9cdcacf6d841f5d11eaeef5a
Truman Cheng <trumanhc@uci.edu>
2020-09-23 04:05:53 -0700

    login form template/view

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from app import app
 from flask import render_template
-
+from app.forms import LoginForm
 
 #decorators
 #associates url from arag to the function below
@@ -21,3 +21,9 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form = form)
