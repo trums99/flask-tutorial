90e0a64f89fd3212f23fa38e63f0904695ddb238
Truman Cheng <trumanhc@uci.edu>
2020-09-25 05:04:59 -0700

    handle form input/validation/better url calling

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect
 from app.forms import LoginForm
 
 #decorators
@@ -23,7 +23,15 @@ def index():
     return render_template('index.html', title='Home', user=user, posts=posts)
 
 
-@app.route('/login')
+#'GET' requests are those that return info to the client
+#'POST' requests are usually when browser submits form data to the server
+
+
+@app.route('/login', methods = ['GET', 'POST']) #accepts get and post, override default
 def login():
     form = LoginForm()
+    if form.validate_on_submit(): #processes the form (submit button pressed)
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form = form)
